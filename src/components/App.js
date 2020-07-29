6df567769112086b8471dc32ad5b3febd3a83365
awpala <adam.w.pala@gmail.com>
2020-07-29 14:10:10 -0500

    Initialize context and components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Vocab from './Vocab';
 
 const App = () => {
     return (
         <div>
             Hello World
+            <Vocab />
         </div>
     );
 }
---
+++ src/components/context/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export const UserContext = React.createContext();
+
+export const Provider = (props) => {
+    // TO-DO: state variables
+
+    // TO-DO: event handlers & actions
+
+    // TO-DO: return component
+}
