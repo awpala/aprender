2c9911d1ae2cd55309cdac9cd163c1441424e415
awpala <adam.w.pala@gmail.com>
2023-06-14 23:18:08 +0000

    ci(github actions): add additional debug prints

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo $HOME
+echo "Executing deploy.sh"
+echo "Current directory: $(pwd)"
+echo "Contents of the current directory: $(ls -la)"
+
+echo "Home path: $HOME"
 git switch master --force
 git pull
 nvm use 14 # client app requires Node v.14
