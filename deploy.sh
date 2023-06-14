eaf4f515f0b8f0ddb8c0d9783c3ca4d0fbf8430d
awpala <adam.w.pala@gmail.com>
2023-06-14 23:33:29 +0000

    ci(github actions): update script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Set environment variables and source files
-source $HOME/.bash_profile
-source $HOME/.bashrc
-
 echo "User's Home directory: $HOME"
 git switch master --force
 git pull
+source $HOME/.bash_profile
+source $HOME/.bashrc
 nvm use 14 # client app requires Node v.14
 yarn install
 yarn build
