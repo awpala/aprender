d93ed271d9731fae8ad4dd4bf25742f3884e0038
awpala <apala7@gatech.edu>
2023-09-26 07:42:08 +0000

    chore(`hosting.sh`): update nginx configs to porkbun

--- hosting.sh
+++ hosting.sh
@@ -5,7 +5,9 @@
 # append NGINX configuration to file `/etc/nginx/sites-available/aprender`,
 # the configuration listens on port 80 and proxies requests to
 # localhost:5555, and sets headers for proxying and upgrading the
-# connection
+# connection. Additionally, subdomain `db.aprender.tech` hosts 
+# pgadmin access to live postgres database via Docker containers running on
+# server as localhost (127.0.0.1).
 sudo echo '
 server {
 
@@ -14,12 +16,51 @@ server {
   server_name  aprender.tech;
 
   location / {
-      proxy_pass http://127.0.0.1:5555;
-      proxy_http_version 1.1;
-      proxy_set_header Upgrade $http_upgrade;
-      proxy_set_header Connection 'upgrade';
-      proxy_set_header Host $host;
-      proxy_cache_bypass $http_upgrade;
+    proxy_pass http://127.0.0.1:5555;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host $host;
+    proxy_cache_bypass $http_upgrade;
+  }
+
+  listen 443 ssl;
+
+  # from porkbun
+  ssl_certificate /etc/nginx/sites-available/ssl-certs-porkbun/aprender.tech/domain.cert.pem;
+  ssl_certificate_key /etc/nginx/sites-available/ssl-certs-porkbun/aprender.tech/private.key.pem;
+}
+
+# pgadmin (HTTP) -- redirect to HTTPS
+server {
+
+  listen 80;
+
+  server_name  db.aprender.tech;
+
+  location / {
+    return 301 https://$host$request_uri;
+  }
+}
+
+# pgadmin (HTTPS)
+server {
+
+  listen 443 ssl;
+
+  server_name  db.aprender.tech;
+
+  # from porkbun
+  ssl_certificate /etc/nginx/sites-available/ssl-certs-porkbun/aprender.tech/domain.cert.pem;
+  ssl_certificate_key /etc/nginx/sites-available/ssl-certs-porkbun/aprender.tech/private.key.pem;
+
+  location / {
+    proxy_pass http://127.0.0.1:8080;
+    proxy_http_version 1.1;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection 'upgrade';
+    proxy_set_header Host $host;
+    proxy_cache_bypass $http_upgrade;
   }
 }
 ' > /etc/nginx/sites-available/aprender;
@@ -31,6 +72,8 @@ sudo ln -s /etc/nginx/sites-available/aprender /etc/nginx/sites-enabled;
 # restart NGINX service to apply configuration changes
 sudo service nginx restart
 
+# BELOW IS OBSOLETE -- managing SSL cert via porkbun as of Sept 2023
+
 # execute Certbot to obtain SSL/TLS cert for domain `aprender.tech` to
 # provide HTTPS security for the domain
-sudo certbot  -d aprender.tech
+# sudo certbot  -d aprender.tech
